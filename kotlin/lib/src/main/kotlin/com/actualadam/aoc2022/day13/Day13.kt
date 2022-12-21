ff2994b6ab0c9938732faacdca31073e2e3b2641
Adam Miller <adam@adammiller.name>
2022-12-20 22:19:57 -0800

    day 13

--- kotlin/lib/src/main/kotlin/com/actualadam/aoc2022/day13/Day13.kt
+++ kotlin/lib/src/main/kotlin/com/actualadam/aoc2022/day13/Day13.kt
@@ -3,7 +3,7 @@ package com.actualadam.aoc2022.day13
 import kotlinx.serialization.json.*
 
 object Day13 {
-    data class TreeNode<T: Comparable<T>>(
+    data class TreeNode<T : Comparable<T>>(
         val value: T,
         val children: MutableList<TreeNode<T>>? = null,
     ) {
@@ -12,64 +12,90 @@ object Day13 {
         }
     }
 
-    private enum class TypeCase{
+    private enum class TypeCase {
         BothIntegers,
         BothLists,
         OneIntegerOneList,
     }
+
     private fun getTypeCase(pair: Pair<JsonElement, JsonElement>): TypeCase =
-        pair.toList().map {it::class.simpleName}.toSet().let {
+        pair.toList().map { it::class.simpleName }.toSet().let {
             if (it.size == 2) {
                 TypeCase.OneIntegerOneList
             } else {
-            when(pair.first) {
-                is JsonArray -> TypeCase.BothLists
-                is JsonPrimitive -> TypeCase.BothIntegers
-                else -> throw IllegalStateException("didn't plan for ${pair.first::class.simpleName}")
-            }
-        }
-    }
-    fun isInOrder(packets: Pair<JsonArray, JsonArray>): Boolean {
-        val zipped = packets.first.zip(packets.second)
-        fun loop(pairs: List<Pair<JsonElement, JsonElement>> = zipped): Boolean {
-            for (pair in pairs) {
-                if (pair.first is JsonPrimitive && pair.second is JsonPrimitive) {
-                    val first = (pair.first as JsonPrimitive).content.toInt()
-                    val second = (pair.second as JsonPrimitive).content.toInt()
-                    if (first < second) return true
-                    else if (first > second) return false
-                    continue; //value are integers but they are the same
-                } else if (pair.first is JsonArray && pair.second is JsonArray) {
-                    val first = (pair.first as JsonArray)
-                    val second = (pair.second as JsonArray)
-                    return loop(first.zip(second))
-                } else {
-                    // todo: can I make a castIf function that takes a reference and a type and checks that the object is that type then returns a reference of that type, or null, or throw?
-                    //todo: I don't know if that would work, and why it wouldn't already exist if that worked
-                    val first = if (pair.first is JsonPrimitive) JsonArray(listOf(pair.first)) else pair.first as JsonArray
-                    val second = if (pair.second is JsonPrimitive) JsonArray(listOf(pair.second)) else pair.second as JsonArray
-                    return loop(first.zip(second))
+                when (pair.first) {
+                    is JsonArray -> TypeCase.BothLists
+                    is JsonPrimitive -> TypeCase.BothIntegers
+                    else -> throw IllegalStateException("didn't plan for ${pair.first::class.simpleName}")
                 }
             }
-            TODO("if we made it all the way through the zipped pairs, compare the size of the zipped pairs to the size of each list and act accordingly")
-            /// I think I need the JsonElements all the way flattened to be able to say.
-
         }
-        return loop()
+
+    fun parse(string: String): JsonElement = Json.parseToJsonElement(string).jsonArray
+
+    val packetComparator = Comparator { left: JsonElement, right: JsonElement ->
+        when(isInOrder(Pair(left, right))) {
+            true -> -1
+            false -> 1
+            null -> 0
+        }
+    }
+
+    fun isInOrder(packetData: Pair<JsonElement, JsonElement>): Boolean? {
+        when (getTypeCase(packetData)) {
+            TypeCase.BothIntegers -> {
+                val left = packetData.first.jsonPrimitive.content.toInt()
+                val right = packetData.second.jsonPrimitive.content.toInt()
+                if (left < right) return true
+                if (left > right) return false
+                return null
+            }
+            TypeCase.BothLists -> {
+                val left = packetData.first.jsonArray
+                val right = packetData.second.jsonArray
+                left.zip(right).forEach {
+                    val res = isInOrder(it)
+                    if (res!= null) return res
+                }
+                if (left.size < right.size) return true
+                if (left.size > right.size) return false
+                return null
+            }
+            TypeCase.OneIntegerOneList -> {
+                val left = if (packetData.first is JsonPrimitive) JsonArray(content = listOf(packetData.first)) else packetData.first
+                val right = if (packetData.second is JsonPrimitive) JsonArray(content = listOf(packetData.second)) else packetData.second
+                val res = isInOrder(Pair(left, right))
+                if (res!= null) return res
+            }
+        }
+        return null
     }
 
     fun part1(lines: List<String>) : Int {
 
-        fun parse(string: String): JsonArray = Json.parseToJsonElement(string).jsonArray
 
         val packetPairs = lines.chunked(3).map { Pair(parse(it[0]), parse(it[1])) }
 
-       return 0
+
+        val validated = packetPairs.mapIndexed { idx, packetPair -> Pair(idx, isInOrder(packetPair)) }
+        return validated
+            .filter { it.second!! }
+            .sumOf{
+                it.first + 1
+            }
     }
     fun part2(lines: List<String>) : Int {
-        return 0
+        val firstDividerPacket = "[[2]]"
+        val secondDividerPacket = "[[6]]"
+        val packets = lines
+            .filterNot { it.isEmpty() }
+            .plus(listOf(firstDividerPacket, secondDividerPacket))
+            .map { parse(it) }
+        val sorted = packets.sortedWith(packetComparator).mapIndexed { idx, packet -> Pair(idx, packet.toString()) }
+        val filtered = sorted
+            .filter { it.second == firstDividerPacket || it.second == secondDividerPacket }
+        return filtered
+            .map { it.first + 1 }
+            .reduce { a, b -> a * b }
     }
-}
-
-abstract class TypedKey<V> {
 }
--- kotlin/lib/src/test/kotlin/com/actualadam/aoc2022/day13/Day13Test.kt
+++ kotlin/lib/src/test/kotlin/com/actualadam/aoc2022/day13/Day13Test.kt
@@ -3,6 +3,7 @@ package com.actualadam.aoc2022.day13
 import com.actualadam.aoc2022.TestUtil
 import com.actualadam.aoc2022.day13.Day13
 import com.actualadam.aoc2022.day13.Day13.isInOrder
+import com.actualadam.aoc2022.day13.Day13.parse
 import com.actualadam.aoc2022.day13.Day13.part1
 import com.actualadam.aoc2022.day13.Day13.part2
 import io.kotest.core.spec.style.FreeSpec
@@ -13,30 +14,21 @@ class Day13Test : FreeSpec({
         val exampleInput = TestUtil.getInputLines("/day13/example.txt")
         val puzzleInput = TestUtil.getInputLines("/day13/puzzle-input.txt")
         "part 1" - {
-            "individual packets" - {
-                "all ints same length" - {
-                    val input = """
-                        [1,1,3,1,1]
-                        [1,1,5,1,1]
-                    """.trimIndent()
-//                    isInOrder(input) shouldBe true
-                }
-            }
             "example" {
-                part1(exampleInput) shouldBe 31
+                part1(exampleInput) shouldBe 13
             }
             "puzzle" {
-                part1(puzzleInput) shouldBe 0
+                part1(puzzleInput) shouldBe 5675
             }
 
         }
 
         "part 2" - {
             "example" {
-                part2(exampleInput) shouldBe 0
+                part2(exampleInput) shouldBe 140
             }
             "puzzle" {
-                part2(puzzleInput) shouldBe 0
+                part2(puzzleInput) shouldBe 20383
             }
         }
     }
